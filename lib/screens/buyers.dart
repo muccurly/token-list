423946f234c087eb3f79ff28e745f735100b13e4
nurbol97 <yessnurbol@gmail.com>
2021-04-24 22:06:01 +0600

    BuyersPage

--- lib/screens/buyers.dart
+++ lib/screens/buyers.dart
@@ -8,91 +8,94 @@ class Buyers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: SafeArea(
-          child: ListView.builder(
-            padding: const EdgeInsets.all(16),
-            itemBuilder: (context, index) {
-              final application = BUYERS[index];
-              return BuyersCard(
-                buyers: application,
-              );
-            },
-            itemCount: BUYERS.length,
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+              child: Text(
+                'Покупатели',
+                style: TextStyle(
+                  color: Color(0xFF4B4B4B),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              height: Global.getSize(context).width / 1.1,
+              child: ListView.builder(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                itemBuilder: (context, index) {
+                  final application = BUYERS[index];
+                  return BuyersCard(
+                    buyers: application,
+                  );
+                },
+                itemCount: BUYERS.length,
+              ),
+            ),
+
+            /// продам сам кнопка
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              height: 55,
+              width: Global.getSize(context).width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ПРОДАМ САМ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Style.orange,
+                  elevation: 0,
+                  padding: const EdgeInsets.only(top: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+
+            /// продайте мне кнопка
+            Container(
+              height: 55,
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              width: Global.getSize(context).width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'ПРОДАЙТЕ МНЕ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Style.blue,
+                  elevation: 0,
+                  padding: const EdgeInsets.only(top: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// child: ListView(
-//             padding: const EdgeInsets.all(16),
-//             children: [
-//               const SizedBox(height: 12),
-//               Text(
-//                 'Покупатели',
-//                 style: TextStyle(
-//                   color: Style.blue,
-//                   fontWeight: FontWeight.w600,
-//                   fontSize: 16,
-//                 ),
-//               ),
-
-//               /// продам сам кнопка
-//               Container(
-//                 height: 55,
-//                 width: Global.getSize(context).width,
-//                 child: ElevatedButton(
-//                   onPressed: () {},
-//                   child: Text(
-//                     'ПРОДАМ САМ',
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 16,
-//                       fontWeight: FontWeight.w700,
-//                     ),
-//                   ),
-//                   style: ElevatedButton.styleFrom(
-//                     primary: Style.orange,
-//                     elevation: 0,
-//                     padding: const EdgeInsets.only(top: 2.0),
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(7),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               const SizedBox(height: 12),
-
-//               /// продайте мне кнопка
-//               Container(
-//                 height: 55,
-//                 width: Global.getSize(context).width,
-//                 child: ElevatedButton(
-//                   onPressed: () {},
-//                   child: Text(
-//                     'ПРОДАЙТЕ МНЕ',
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.w700,
-//                       fontSize: 16,
-//                     ),
-//                   ),
-//                   style: ElevatedButton.styleFrom(
-//                     primary: Style.blue,
-//                     elevation: 0,
-//                     padding: const EdgeInsets.only(top: 2.0),
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(7),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
 
 ///Buyers Card
 class BuyersCard extends StatelessWidget {
@@ -109,27 +112,33 @@ class BuyersCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: Container(
-        padding: EdgeInsets.fromLTRB(32, 19, 18, 14),
+        padding: EdgeInsets.fromLTRB(20, 16, 15, 14),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             /// top section
             Text(
-              'Заявка на ${buyers['name']}',
+              buyers['name'],
               style: TextStyle(
-                color: Style.orange,
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
+                color: Style.blue,
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
               ),
+            ),
+            SizedBox(
+              height: 4,
             ),
 
             /// middle section
             Text(
               buyers['address'],
               style: TextStyle(
-                fontSize: 10,
-                fontWeight: FontWeight.w300,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
               ),
+            ),
+            SizedBox(
+              height: 4,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -146,7 +155,8 @@ class BuyersCard extends StatelessWidget {
                   '${DateFormat('kk:mm, dd.MM.yyyy').format(buyers['date'])}',
                   style: TextStyle(
                     fontSize: 10,
-                    fontWeight: FontWeight.w300,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
                   ),
                 ),
               ],

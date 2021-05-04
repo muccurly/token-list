9b6193dc4b8b42c783056c3693eade71535a641d
Mikhail Krassavin <mikhail.krassavin@gmail.com>
2021-05-04 22:58:26 +0600

    home: trying out fixed icon and filter

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -41,21 +41,127 @@ class _HomeScreenState extends State<HomeScreen> {
         endDrawer: DrawerWrapper(type: DrawerType.filter),
         endDrawerEnableOpenDragGesture: false,
         body: SafeArea(
-          child: PageView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: advert.length,
-            controller: PageController(
-              initialPage: 0,
-              viewportFraction: 1,
+            child: Stack(
+          children: [
+            Positioned.fill(
+              child: PageView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: advert.length,
+                controller: PageController(
+                  initialPage: 0,
+                  viewportFraction: 1,
+                ),
+                itemBuilder: (context, index) {
+                  return HomeScreenCard(
+                    advert: advert[index],
+                    sKey: _sKey,
+                  );
+                },
+              ),
             ),
-            itemBuilder: (context, index) {
-              return HomeScreenCard(
-                advert: advert[index],
-                sKey: _sKey,
-              );
-            },
-          ),
-        ),
+
+            /// header
+            Positioned(
+              top: Global.getViewPadding(context).top,
+              left: 0,
+              right: 16,
+              child: Row(
+                children: [
+                  /// jurta image
+                  Container(
+                    height: 34,
+                    // width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                      color: Style.blue,
+                    ),
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      color: Colors.white,
+                      height: 26,
+                    ),
+                  ),
+                  Spacer(),
+
+                  /// filter old
+                  /* Expanded(
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            height: 30,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              children: _headerTexts.map(
+                                (text) {
+                                  return Row(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            _headerSelected = text;
+                                          });
+                                        },
+                                        child: Text(
+                                          text,
+                                          style: TextStyle(
+                                            color: _headerSelected == text
+                                                ? Colors.white
+                                                : Colors.grey.shade300,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ),
+                                      if (text != _headerTexts[2])
+                                        Container(
+                                          width: 1,
+                                          height: 20,
+                                          color: Colors.white,
+                                          margin:
+                                              const EdgeInsets.symmetric(horizontal: 8),
+                                        ),
+                                    ],
+                                  );
+                                },
+                              ).toList(),
+                            ),
+                          ),
+                        ), */
+
+                  /// filter new
+                  GestureDetector(
+                    onTap: () {
+                      _sKey.currentState.openEndDrawer();
+                    },
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Фильтр',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        ImageIcon(
+                          AssetImage('assets/images/sort.png'),
+                          color: Colors.white,
+                          size: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
@@ -129,107 +235,6 @@ class _HomeScreenCardState extends State<HomeScreenCard> {
                 end: Alignment.topCenter,
               ),
             ),
-          ),
-        ),
-
-        /// header
-        Positioned(
-          top: Global.getViewPadding(context).top + 16,
-          left: 0,
-          right: 16,
-          child: Row(
-            children: [
-              /// jurta image
-              Container(
-                height: 34,
-                // width: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
-                  ),
-                  color: Style.blue,
-                ),
-                alignment: Alignment.centerRight,
-                padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  color: Colors.white,
-                  height: 26,
-                ),
-              ),
-              Spacer(),
-
-              /// filter old
-              /* Expanded(
-                          child: Container(
-                            alignment: Alignment.centerRight,
-                            height: 30,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              children: _headerTexts.map(
-                                (text) {
-                                  return Row(
-                                    children: [
-                                      GestureDetector(
-                                        onTap: () {
-                                          setState(() {
-                                            _headerSelected = text;
-                                          });
-                                        },
-                                        child: Text(
-                                          text,
-                                          style: TextStyle(
-                                            color: _headerSelected == text
-                                                ? Colors.white
-                                                : Colors.grey.shade300,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                      ),
-                                      if (text != _headerTexts[2])
-                                        Container(
-                                          width: 1,
-                                          height: 20,
-                                          color: Colors.white,
-                                          margin:
-                                              const EdgeInsets.symmetric(horizontal: 8),
-                                        ),
-                                    ],
-                                  );
-                                },
-                              ).toList(),
-                            ),
-                          ),
-                        ), */
-
-              /// filter new
-              GestureDetector(
-                onTap: () {
-                  widget.sKey.currentState.openEndDrawer();
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Фильтр',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    const SizedBox(width: 4),
-                    ImageIcon(
-                      AssetImage('assets/images/sort.png'),
-                      color: Colors.white,
-                      size: 24,
-                    ),
-                  ],
-                ),
-              ),
-            ],
           ),
         ),
 

86062a58d5e4e3070d10c0e2285dac82d1aee626
ushamiev <ushamiev.m@gmail.com>
2021-09-08 18:20:45 +0600

    env config android

---
+++ lib/src/env_config.dart
@@ -0,0 +1,29 @@
+class EnvironmentConfig {
+  static const APP_NAME = String.fromEnvironment(
+      'EXAMPLE_APP_NAME',
+      defaultValue: 'Jurta'
+  );
+  static const APP_SUFFIX = String.fromEnvironment(
+      'EXAMPLE_APP_SUFFIX'
+  );
+  static const API_URL_DM = String.fromEnvironment(
+      'EXAMPLE_API_URL_DM',
+      defaultValue: 'https://dm-dev.jurta.kz'
+  );
+  static const API_URL_VM = String.fromEnvironment(
+      'EXAMPLE_API_URL_VM',
+      defaultValue: 'https://vm-dev.jurta.kz'
+  );
+  static const API_URL_GM = String.fromEnvironment(
+      'EXAMPLE_API_URL_GM',
+      defaultValue: 'https://gm-dev.jurta.kz'
+  );
+  static const API_URL_UM = String.fromEnvironment(
+      'EXAMPLE_API_URL_UM',
+      defaultValue: 'https://um-dev.jurta.kz'
+  );
+  static const API_VERSION = String.fromEnvironment(
+      'EXAMPLE_API_VERSION',
+      defaultValue: 'open-api'
+  );
+}
